b8bfc2405f82524622212ca13bf85a4c9870175f
Rahmatul Ramadhani <rahmatulramadhani06@gmail.com>
2025-06-11 10:20:37 +0700

    update smk3

--- database/migrations/2025_05_09_163524_create_penerapan_smk3_table.php
+++ database/migrations/2025_05_09_163524_create_penerapan_smk3_table.php
@@ -13,17 +13,11 @@ return new class extends Migration
             $table->year('tahun');
             $table->tinyInteger('bulan')->comment('1-12');
             $table->string('provinsi');
-            $table->string('kbli');
-            $table->string('kategori_penilaian')->comment('awal, transisi, lanjutan');
-            $table->string('tingkat_pencapaian')->comment('baik, memuaskan');
-            $table->string('jenis_penghargaan');
             $table->integer('jumlah_perusahaan')->default(0);
             $table->timestamps();
 
             $table->index(['tahun', 'bulan']);
             $table->index('provinsi');
-            $table->index('kbli');
-            $table->index('kategori_penilaian');
         });
     }
 
